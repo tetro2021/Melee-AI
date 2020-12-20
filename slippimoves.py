874bd16f8f314f51a76902632367148b3395d8d1
kanex217 <kanex217@umn.edu>
2020-12-20 16:28:56 -0600

    Add files via upload

--- slippimoves.py
+++ slippimoves.py
@@ -50,40 +50,40 @@ def SDIRight(ai_state, controller):
 
 
 def SDIDownLeft(ai_state, controller):
-    if ai_state.action == enums.Action.THROWN_UP:
-        #controller.release_all()
-        if controller.current.main_stick != (0,0):
-            #print("got here")
-            controller.tilt_analog(enums.Button.BUTTON_MAIN, 0, 0)
-    elif ai_state.hitstun_frames_left != 0:
+    # if ai_state.action == enums.Action.THROWN_UP:
+    #     #controller.release_all()
+    #     if controller.current.main_stick != (0,0):
+    #         #print("got here")
+    #         controller.tilt_analog(enums.Button.BUTTON_MAIN, 0, 0)
+    if ai_state.hitlag:
         if (controller.current.main_stick != (0,0)):
-            controller.release_all()
+            #controller.release_all()
             controller.tilt_analog(enums.Button.BUTTON_MAIN, 0, 0)
             #print(controller.current.main_stick)
         else:
-            controller.release_all()
+            #controller.release_all()
             controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.5, 0.5)
             #print(controller.current.main_stick)
     else:
-        controller.release_all()
+        controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.5, 0.5)
 
 def SDIDownRight(ai_state, controller):
-    if ai_state.action == enums.Action.THROWN_UP:
-        #controller.release_all()
-        if controller.current.main_stick != (0,0):
-            #print("got here")
-            controller.tilt_analog(enums.Button.BUTTON_MAIN, 0, 0)
-    elif ai_state.hitstun_frames_left != 0:
+    # if ai_state.action == enums.Action.THROWN_UP:
+    #     #controller.release_all()
+    #     if controller.current.main_stick != (0,0):
+    #         #print("got here")
+    #         controller.tilt_analog(enums.Button.BUTTON_MAIN, 1, 0)
+    if ai_state.hitlag:
         if (controller.current.main_stick != (1,0)):
-            controller.release_all()
+            #controller.release_all()
             controller.tilt_analog(enums.Button.BUTTON_MAIN, 1, 0)
             #print(controller.current.main_stick)
         else:
-            controller.release_all()
+            #controller.release_all()
             controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.5, 0.5)
             #print(controller.current.main_stick)
     else:
-        controller.release_all()
+        controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.5, 0.5)
 
 
 def faceOpponent(ai_state, controller, player_state):
@@ -101,23 +101,19 @@ def faceOpponent(ai_state, controller, player_state):
 
 def simpleFoxAI(ai_state, controller, player_state):
     if ai_state.on_ground:
-                if ai_state.facing and (ai_state.x > player_state.x):
-                    #print("Facing right flip left")
-                    controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.4,0.5)
-                elif not ai_state.facing and (ai_state.x < player_state.x):
-                    #print("Facing left flip right")
-                    controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.6,0.5)
-                else:
-                    if ai_state.x < player_state.x:
-                        SDIDownLeft(ai_state,controller)
-                    else:
-                        SDIDownRight(ai_state,controller)
-                    shortHopLaser(ai_state,controller)
+        if ai_state.facing and (ai_state.x > player_state.x):
+            #print("Facing right flip left")
+            controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.4,0.5)
+        elif not ai_state.facing and (ai_state.x < player_state.x):
+            #print("Facing left flip right")
+            controller.tilt_analog(enums.Button.BUTTON_MAIN, 0.6,0.5)
+        else:
+            if ai_state.x < player_state.x:
+                SDIDownLeft(ai_state,controller)
+            else:
+                SDIDownRight(ai_state,controller)
+            shortHopLaser(ai_state,controller)
     else:
-        if ai_state.action == enums.Action.TUMBLING:
-            controller.press_shoulder(enums.Button.BUTTON_L, )
-            print("Got here")
-            return
         if ai_state.x < player_state.x:
             SDIDownLeft(ai_state,controller)
         else:
@@ -129,7 +125,30 @@ def simpleFoxAI(ai_state, controller, player_state):
         #controller.release_all()
 
 
+def PoorlyMadeTechplacement(ai_state, controller, stagePositiony):
+    if ai_state.on_ground:
+            if ai_state.y < stagePositiony:
+                stagePositiony = ai_state.y
+                print(stagePositiony)
+            if ai_state.y < stagePositiony:
+                #print("This is stage position: ")
+                #print(stagePositiony)
+                #print("this is ai_state")
+                #print(ai_state.y)
+                controller.press_button(enums.Button.BUTTON_L)
+            else:
+                #print("This is stage position BIGGER: ")
+                #print(stagePositiony)
+                #print("this is ai_state SMALLER")
+                #print(ai_state.y)
+                controller.release_button(enums.Button.BUTTON_L)
 
+def SimpleTech(ai_state, controller):
+    if ai_state.y < 0 and not ai_state.on_ground and not ai_state.off_stage and not ai_state.hitlag:
+        controller.release_all()
+        controller.press_button(enums.Button.BUTTON_L)
+    else:
+        controller.release_button(enums.Button.BUTTON_L)
 
 # def ComboDILeft(ai_state, controller):
 #     if ai_state.action == enums.Action.THROWN_UP:
